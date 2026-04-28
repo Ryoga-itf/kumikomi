8ec6004650a8179d50110f38ae13be6f629ce2af
Ryoga-exe <git@ryoga.dev>
2026-04-28 16:15:31 +0900

    feat: IntentSample

--- 04_IntentSample/app/src/main/java/com/example/intentsample/LocationActivity.kt
+++ 04_IntentSample/app/src/main/java/com/example/intentsample/LocationActivity.kt
@@ -1,41 +1,157 @@
 package com.example.intentsample
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
+import android.location.LocationProvider
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import com.example.intentsample.databinding.ActivityLocationBinding
+import androidx.core.app.ActivityCompat
+import androidx.core.graphics.Insets
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
-class LocationActivity : AppCompatActivity() {
+class LocationActivity : AppCompatActivity(), LocationListener, View.OnClickListener {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityLocationBinding
+    private lateinit var locationManager: LocationManager
+
+    private lateinit var wifiLatitudeTextView: TextView
+    private lateinit var wifiLongitudeTextView: TextView
+    private lateinit var wifiAccuracyTextView: TextView
+    private lateinit var wifiAltitudeTextView: TextView
+
+    private lateinit var gpsLatitudeTextView: TextView
+    private lateinit var gpsLongitudeTextView: TextView
+    private lateinit var gpsAccuracyTextView: TextView
+    private lateinit var gpsAltitudeTextView: TextView
+
+    private var locationType: Int = WIFI
+
+    companion object {
+        private const val WIFI = 0
+        private const val GPS = 1
+        private const val TAG = "PlaceSample"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_location)
 
-        binding = ActivityLocationBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars: Insets = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
 
-        setSupportActionBar(binding.toolbar)
+        wifiLatitudeTextView = findViewById(R.id.text_view_wifi_latitude_value)
+        wifiLongitudeTextView = findViewById(R.id.text_view_wifi_longitude_value)
+        wifiAccuracyTextView = findViewById(R.id.text_view_wifi_accuracy_value)
+        wifiAltitudeTextView = findViewById(R.id.text_view_wifi_altitude_value)
 
-        val navController = findNavController(R.id.nav_host_fragment_content_location)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
+        gpsLatitudeTextView = findViewById(R.id.text_view_gps_latitude_value)
+        gpsLongitudeTextView = findViewById(R.id.text_view_gps_longitude_value)
+        gpsAccuracyTextView = findViewById(R.id.text_view_gps_accuracy_value)
+        gpsAltitudeTextView = findViewById(R.id.text_view_gps_altitude_value)
 
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null)
-                .setAnchorView(R.id.fab).show()
+        val gpsButton: Button = findViewById(R.id.button_gps)
+        gpsButton.setOnClickListener(this)
+
+        val wifiButton: Button = findViewById(R.id.button_wifi)
+        wifiButton.setOnClickListener(this)
+
+        locationManager = getSystemService(LOCATION_SERVICE) as LocationManager
+    }
+
+    override fun onResume() {
+        Log.d(TAG, "plog onResume")
+        super.onResume()
+    }
+
+    override fun onPause() {
+        Log.d(TAG, "plog onPause")
+        if (::locationManager.isInitialized && hasLocationPermission()) {
+            locationManager.removeUpdates(this)
+        }
+        super.onPause()
+    }
+
+    override fun onLocationChanged(location: Location) {
+        Log.d(TAG, "plog onLocationChanged")
+
+        when (locationType) {
+            GPS -> {
+                gpsLatitudeTextView.text = location.latitude.toString()
+                gpsLongitudeTextView.text = location.longitude.toString()
+                gpsAccuracyTextView.text = location.accuracy.toString()
+                gpsAltitudeTextView.text = location.altitude.toString()
+            }
+
+            WIFI -> {
+                wifiLatitudeTextView.text = location.latitude.toString()
+                wifiLongitudeTextView.text = location.longitude.toString()
+                wifiAccuracyTextView.text = location.accuracy.toString()
+                wifiAltitudeTextView.text = location.altitude.toString()
+            }
+        }
+
+        if (hasLocationPermission()) {
+            locationManager.removeUpdates(this)
         }
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_location)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
+    @Suppress("DEPRECATION")
+    override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
+        when (status) {
+            LocationProvider.AVAILABLE -> Log.v(TAG, "AVAILABLE")
+            LocationProvider.OUT_OF_SERVICE -> Log.v(TAG, "OUT_OF_SERVICE")
+            LocationProvider.TEMPORARILY_UNAVAILABLE -> Log.v(TAG, "TEMPORARILY_UNAVAILABLE")
+        }
+    }
+
+    override fun onProviderEnabled(provider: String) {}
+
+    override fun onProviderDisabled(provider: String) {}
+
+    override fun onClick(view: View) {
+        if (!hasLocationPermission()) return
+
+        when (view.id) {
+            R.id.button_gps -> {
+                locationType = GPS
+                locationManager.requestLocationUpdates(
+                    LocationManager.GPS_PROVIDER,
+                    0L,
+                    0f,
+                    this
+                )
+            }
+
+            R.id.button_wifi -> {
+                locationType = WIFI
+                locationManager.requestLocationUpdates(
+                    LocationManager.NETWORK_PROVIDER,
+                    0L,
+                    0f,
+                    this
+                )
+            }
+        }
+    }
+
+    private fun hasLocationPermission(): Boolean {
+        return ActivityCompat.checkSelfPermission(
+            this,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED ||
+                ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ) == PackageManager.PERMISSION_GRANTED
     }
 }
--- 04_IntentSample/app/src/main/java/com/example/intentsample/OrientationActivity.kt
+++ 04_IntentSample/app/src/main/java/com/example/intentsample/OrientationActivity.kt
@@ -1,41 +1,124 @@
 package com.example.intentsample
 
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import com.example.intentsample.databinding.ActivityOrientationBinding
+import androidx.core.graphics.Insets
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import kotlin.math.floor
 
-class OrientationActivity : AppCompatActivity() {
+class OrientationActivity : AppCompatActivity(), SensorEventListener {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityOrientationBinding
+    private lateinit var checkBoxOrientation: CheckBox
+    private lateinit var azimuthText: TextView
+    private lateinit var pitchText: TextView
+    private lateinit var rollText: TextView
+
+    private lateinit var sensorManager: SensorManager
+    private var accelerationSensor: Sensor? = null
+    private var magneticSensor: Sensor? = null
+
+    private var accelerationValue = FloatArray(3)
+    private var geoMagneticValue = FloatArray(3)
+    private val orientationValue = FloatArray(3)
+    private val inRotationMatrix = FloatArray(9)
+    private val outRotationMatrix = FloatArray(9)
+    private val inclinationMatrix = FloatArray(9)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_orientation)
 
-        binding = ActivityOrientationBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars: Insets = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
 
-        setSupportActionBar(binding.toolbar)
+        checkBoxOrientation = findViewById(R.id.checkbox_orientation)
+        azimuthText = findViewById(R.id.text_view_azimuth)
+        rollText = findViewById(R.id.text_view_roll)
+        pitchText = findViewById(R.id.text_view_pitch)
+        val buttonOrientation: Button = findViewById(R.id.button_orientation)
 
-        val navController = findNavController(R.id.nav_host_fragment_content_orientation)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
+        sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+        accelerationSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        magneticSensor = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD)
 
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null)
-                .setAnchorView(R.id.fab).show()
+        buttonOrientation.setOnClickListener {
+            if (!checkBoxOrientation.isChecked) {
+                updateOrientation()
+            }
         }
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_orientation)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
+    override fun onResume() {
+        super.onResume()
+        accelerationSensor?.also {
+            sensorManager.registerListener(this, it, SensorManager.SENSOR_DELAY_UI)
+        }
+        magneticSensor?.also {
+            sensorManager.registerListener(this, it, SensorManager.SENSOR_DELAY_UI)
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        sensorManager.unregisterListener(this)
+    }
+
+    override fun onSensorChanged(event: SensorEvent) {
+        when (event.sensor.type) {
+            Sensor.TYPE_MAGNETIC_FIELD -> {
+                geoMagneticValue = event.values.clone()
+            }
+            Sensor.TYPE_ACCELEROMETER -> {
+                accelerationValue = event.values.clone()
+            }
+        }
+
+        if (checkBoxOrientation.isChecked) {
+            updateOrientation()
+        }
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+    }
+
+    private fun updateOrientation() {
+        val success = SensorManager.getRotationMatrix(
+            inRotationMatrix,
+            inclinationMatrix,
+            accelerationValue,
+            geoMagneticValue
+        )
+
+        if (!success) return
+
+        SensorManager.remapCoordinateSystem(
+            inRotationMatrix,
+            SensorManager.AXIS_X,
+            SensorManager.AXIS_Z,
+            outRotationMatrix
+        )
+
+        SensorManager.getOrientation(outRotationMatrix, orientationValue)
+
+        val azimuth = floor(Math.toDegrees(orientationValue[0].toDouble())).toInt().toString()
+        val pitch = floor(Math.toDegrees(orientationValue[1].toDouble())).toInt().toString()
+        val roll = floor(Math.toDegrees(orientationValue[2].toDouble())).toInt().toString()
+
+        azimuthText.text = azimuth
+        pitchText.text = pitch
+        rollText.text = roll
     }
 }
