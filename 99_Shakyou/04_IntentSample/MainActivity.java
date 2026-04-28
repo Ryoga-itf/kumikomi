9cae9ad4cdbc24e1b9c518f51b0d43fe1a56c7bc
Ryoga-exe <git@ryoga.dev>
2026-04-28 15:53:44 +0900

    feat: IntentSample 写経

---
+++ 99_Shakyou/04_IntentSample/MainActivity.java
@@ -0,0 +1,44 @@
+import androidx.core.graphics.Insets;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.intentsample.LocationActivity;
+import com.example.intentsample.OrientationActivity;
+import com.example.intentsample.R;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Button orientationActivitySwitchButton = (Button) findViewById(R.id.button_orientation);
+        orientationActivitySwitchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                Intent intent = new Intent(getApplicationContext(), OrientationActivity.class);
+                startActivity(intent);
+            }
+        });
+        Button locationActivitySwitchButton = (Button) findViewById(R.id.button_location);
+        locationActivitySwitchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                Intent intent = new Intent(getApplicationContext(), LocationActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
